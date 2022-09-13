5ecc42298e4b73e313b1bce27b6b5b74a10f3e69
Dmytro <db.ituplink@gmail.com>
2022-09-13 12:05:28 +0300

    All File in folder find method added

--- FindDublicate/Program.cs
+++ FindDublicate/Program.cs
@@ -1,2 +1,46 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+
+string FullPath = "D:/Models material/Substance Painter Alpha";
+
+
+static List<string> GetAllFilesInDirectory(string targetDirectory, List<string> list = null)
+{
+
+    if (list == null)
+    {
+        list = new List<string>();
+    }
+
+
+    string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+    foreach (string fileName in fileEntries)
+    {
+        list.Add(fileName);
+    }
+
+    string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+
+    foreach (string subdirectory in subdirectoryEntries)
+        GetAllFilesInDirectory(subdirectory, list);
+
+    return list;
+}
+
+List<string> AllFile = GetAllFilesInDirectory(FullPath);
+Console.WriteLine($"Find {AllFile.Count} files");
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+
+
+
+
+
+watch.Stop();
+Console.WriteLine($"Execution Time: {watch} s");
+
+
+
